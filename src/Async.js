74acd0b1e99e0b893d4555fb5621e3543abec4e7
Vaibhava_Dwivedi <dwivedivaibhava@gmail.com>
2023-04-14 03:03:25 +0530

    now added all data in map for async instead of showing only one component

--- src/Async.js
+++ src/Async.js
@@ -7,21 +7,29 @@ const apiData = "https://fakestoreapi.com/products";
 // Create Async function to fetch data from the API
 function Async() {
   // Declare state variables to store fetched data
-  const [title, setTitle] = useState([]);
-  const [image, setImage] = useState([]);
-  const [price, setPrice] = useState([]);
-  const [description, setDescription] = useState([]);
+  const [data, setData] = useState([]);
+  // const [title, setTitle] = useState([]);
+  // const [image, setImage] = useState([]);
+  // const [price, setPrice] = useState([]);
+  // const [description, setDescription] = useState([]);
 
   // Use useEffect hook to call the API when the component mounts
+  // useEffect(() => {
+  //   async function getStoreApi() {
+  //     const response = await axios.get(`${apiData}/4`);  //this is to call only one data using id of that data
+  //     setTitle(response.data.title);
+  //     setImage(response.data.image);
+  //     setPrice(response.data.price);
+  //     setDescription(response.data.description);
+  //   }
+  //   getStoreApi();
+  // }, []);
+
   useEffect(() => {
     async function getStoreApi() {
       // Make an API call using axios
-      const response = await axios.get(`${apiData}/4`);
-      // Set the state variables with the response data
-      setTitle(response.data.title);
-      setImage(response.data.image);
-      setPrice(response.data.price);
-      setDescription(response.data.description);
+      const response = await axios.get(apiData);
+      setData(response.data);
     }
     // Call the getStoreApi function
     getStoreApi();
@@ -34,10 +42,25 @@ function Async() {
         <Link to="/">Go Back</Link>
       </span>
       <h1>Async Await method</h1>
-      <h2>{title}</h2>
-      <img src={image} alt="img" />
-      <h4>{price}</h4>
-      <p>{description}</p>
+      <table border="1">
+        <tr>
+          <td>title</td>
+          <td>image</td>
+          <td>price</td>
+          <td>description</td>
+        </tr>
+        {/* mapping through the data state and displaying the data in table format */}
+        {data.map((item) => (
+          <tr>
+            <td>{item.title}</td>
+            <td>
+              <img alt="pic" src={item.image} />
+            </td>
+            <td>{item.price}</td>
+            <td>{item.description}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
